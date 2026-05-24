099afb58d59d4ee9710c9fa54ef1022d2ee61415
rocketmike12 <mg.korop@gmail.com>
2026-05-24 13:52:22 +0300

    handlers updated

--- handlers/auth.js
+++ handlers/auth.js
@@ -1,59 +1,63 @@
 import jwt from "jsonwebtoken";
 
-import { addUser, getOperations, addOperation, delOperation } from "../db/db.js";
+import { getUser, addUser, getOperations, addOperation, delOperation } from "../db/db.js";
+
+const validateUserData = function ({ username, password }) {
+	return typeof username == "string" && username.length >= 3 && /^[a-zA-Z0-9_]{3,32}$/.test(username) && typeof password == "string" && password.length >= 3;
+};
+
+const validateOperationData = function ({ date, category, subcategory, sum }) {
+	return !isNaN(new Date(date).getTime()) && typeof category == "string" && category.length > 0 && typeof subcategory == "string" && subcategory.length > 0 && typeof sum == "number";
+};
 
 export const cookieOpts =
-	process.env.NODE_ENV === "dev" ? { maxAge: 1000 * 60 * 60 * 24 * 2, httpOnly: true } : { maxAge: 1000 * 60 * 60 * 24 * 2, httpOnly: true, sameSite: "none", secure: true, partitioned: true };
+	process.env.NODE_ENV === "dev" ? { maxAge: 1000 * 60 * 60 * 24 * 2, httpOnly: true, path: "/" } : { maxAge: 1000 * 60 * 60 * 24 * 2, httpOnly: true, path: "/", sameSite: "none", secure: true };
 
 export const authenticateToken = async function (req, res, next) {
 	const authCookie = req.cookies["authcookie"];
 
-	if (!authCookie) return res.status(401).send("401 unauthorized");
+	if (!authCookie) {
+		return res.sendStatus(401);
+	}
 
-	jwt.verify(authCookie, process.env.ACCESS_TOKEN_SECRET, async (err, username) => {
-		if (err) return res.status(403).send("403 access denied");
+	try {
+		const payload = jwt.verify(authCookie, process.env.ACCESS_TOKEN_SECRET);
 
-		try {
-			let operations = await getOperations(username);
-
-			if (!operations) throw new Error(operations);
-
-			req.user = { username: username, operations: operations };
-		} catch (err) {
-			res.set("Content-Type", "text/plain");
-
-			if (err.message === "login incorrect") {
-				return res.status(401).send("401 unauthorized: login incorrect");
-			}
-
-			console.error(err);
-			return res.status(406).send(err);
-		}
+		req.user = { username: payload.username };
 
 		next();
-	});
+	} catch (err) {
+		console.error(err);
+
+		return res.sendStatus(403);
+	}
 };
 
 export const loginHandler = async function (req, res) {
 	const { username, password } = req.body;
 
 	try {
-		let userData = await getUser(username, password);
-
-		if (!userData) throw new Error(userData);
-
-		const token = jwt.sign(userData.username, process.env.ACCESS_TOKEN_SECRET);
-		res.cookie("authcookie", token, { maxAge: 900000, httpOnly: true });
-
-		return res.status(200).json({ username: userData.username, operations: userData.operations });
-	} catch (err) {
-		res.set("Content-Type", "text/plain");
-
-		if (err.message === "login incorrect") {
-			return res.status(401).send("401 unauthorized: login incorrect");
+		if (!validateUserData({ username, password })) {
+			return res.status(400).json({
+				error: "invalid data"
+			});
 		}
 
-		console.error(`failed to get user: ${err.message}`);
+		let userData = await getUser(username, password);
+
+		if (!userData) {
+			return res.sendStatus(401);
+		}
+
+		const token = jwt.sign({ username: userData.username }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "2d" });
+		res.cookie("authcookie", token, cookieOpts);
+
+		return res.status(200).json({
+			username: userData.username,
+			operations: userData.operations
+		});
+	} catch (err) {
+		console.error(`failed to get user:`, err);
 		return res.sendStatus(500);
 	}
 };
@@ -62,36 +66,41 @@ export const registerHandler = async function (req, res) {
 	const { username, password } = req.body;
 
 	try {
+		if (!validateUserData({ username, password })) {
+			return res.status(400).json({
+				error: "invalid data"
+			});
+		}
+
 		const userData = await addUser(username, password);
 
-		const token = jwt.sign(userData.username, process.env.ACCESS_TOKEN_SECRET);
+		const token = jwt.sign({ username: userData.username }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "2d" });
 		res.cookie("authcookie", token, cookieOpts);
 
-		return res.status(200).json({ username: userData.username, operations: userData.operations });
+		return res.status(200).json({
+			username: userData.username,
+			operations: userData.operations
+		});
 	} catch (err) {
-		res.set("Content-Type", "text/plain");
-
 		if (err.message === "user already exists") {
 			return res.sendStatus(409);
 		}
 
-		console.error(`user ${username} not created: ${err.message}`);
+		console.error(`user ${username} not created:`, err);
 		return res.sendStatus(500);
 	}
 };
 
 export const sessionHandler = function (req, res) {
-	res.set("Content-Type", "text/plain");
+	res.set("Content-Type", "application/json");
 
-	const token = jwt.sign(req.user.username, process.env.ACCESS_TOKEN_SECRET);
+	const token = jwt.sign({ username: req.user.username }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "2d" });
 	res.cookie("authcookie", token, cookieOpts);
 
 	return res.status(200).json(req.user);
 };
 
 export const logoutHandler = async function (_, res) {
-	res.set("Content-Type", "text/plain");
-
 	res.clearCookie("authcookie", cookieOpts);
 
 	res.sendStatus(200);
@@ -100,6 +109,18 @@ export const logoutHandler = async function (_, res) {
 export const operationHandler = async (req, res) => {
 	try {
 		const { operation } = req.body;
+
+		if (!operation) {
+			return res.status(400).json({
+				error: "operation missing"
+			});
+		}
+
+		if (!validateOperationData(operation)) {
+			return res.status(400).json({
+				error: "invalid data"
+			});
+		}
 
 		const userData = await addOperation(req.user.username, operation);
 
@@ -116,6 +137,12 @@ export const operationHandler = async (req, res) => {
 export const delOperationHandler = async (req, res) => {
 	try {
 		const { operationId } = req.body;
+
+		if (!operationId) {
+			return res.status(400).json({
+				error: "operationId missing"
+			});
+		}
 
 		const userData = await delOperation(req.user.username, operationId);
 
