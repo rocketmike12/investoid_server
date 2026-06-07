f393ec5b7c654254c319526481a8821df040ba72
rocketmike12 <mg.korop@gmail.com>
2026-06-07 13:08:59 +0300

    rewrote main to typescript

---
+++ src/main.ts
@@ -0,0 +1,49 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors, { type CorsOptions } from "cors";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+import { connectDb } from "./config/connectDb";
+import mongoose from "mongoose";
+
+import { authenticateToken, loginHandler, registerHandler, sessionHandler, logoutHandler, operationHandler, delOperationHandler } from "./handlers/auth";
+
+let whitelist = ["http://localhost:5173", "https://rocketmike12.github.io"];
+
+const corsOpts: CorsOptions = {
+	origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+		if (!origin) {
+			return callback(null, true);
+		}
+
+		if (whitelist.includes(origin)) {
+			return callback(null, true);
+		}
+
+		return callback(null, false);
+	},
+	credentials: true
+};
+
+const app = express();
+
+app.use(cors(corsOpts));
+app.use(express.json());
+app.use(cookieParser());
+
+connectDb();
+
+app.post("/api/v0/auth/login/", loginHandler);
+app.post("/api/v0/auth/register/", registerHandler);
+app.post("/api/v0/auth/session/", authenticateToken, sessionHandler);
+app.post("/api/v0/auth/logout/", authenticateToken, logoutHandler);
+app.post("/api/v0/auth/operation/add", authenticateToken, operationHandler);
+app.post("/api/v0/auth/operation/del", authenticateToken, delOperationHandler);
+
+mongoose.connection.once("open", () => {
+	app.listen(8080, () => {
+		console.log("SRV: server is running on :8080");
+	});
+});
