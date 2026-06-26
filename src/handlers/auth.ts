f7226d5a9f54fb5a8e460eacc89924e82935b039
rocketmike12 <mg.korop@gmail.com>
2026-06-26 21:21:56 +0300

    clearCookie fix

--- src/handlers/auth.ts
+++ src/handlers/auth.ts
@@ -115,7 +115,7 @@ export const sessionHandler = async function (req: AuthRequest, res: Response) {
 };
 
 export const logoutHandler = async function (_: AuthRequest, res: Response) {
-	res.clearCookie("authcookie");
+	res.clearCookie("authcookie", cookieOpts);
 
 	res.sendStatus(200);
 };
